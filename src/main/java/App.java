983b2bbde79a513f35cf405e98c93162b1cd8684
Zenas Muli <zenas2294@gmail.com>
2019-05-19 14:31:54 +0300

    Create the first get request in App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Map;
 import java.util.HashMap;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
@@ -7,6 +9,13 @@ public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
         String layout = "templates/layout.vtl";
+        String index = "templates/index.vtl";
+
+        get("/", ((request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", index);
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine()));
 
     }
 }
