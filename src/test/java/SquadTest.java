c5ee8e02839fa22dd2afbd9345e7b0a38231edd1
Zenas Muli <zenas2294@gmail.com>
2019-05-20 02:38:42 +0300

    Test different properties of Squad object

--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -6,8 +6,26 @@ import static org.junit.Assert.*;
 public class SquadTest {
 
     @Test
-    public void Hero_instantiates_true() {
+    public void Squad_instantiates_true() {
         Squad testSquad = new Squad("Olympus", 5, "Fight the titans");
         assertEquals(true, testSquad instanceof Squad);
+    }
+
+    @Test
+    public void Squad_squadName_isString() {
+        Squad testSquad = new Squad("Olympus", 5, "Fight the titans");
+        assertEquals("Olympus", testSquad.getmSquadName());
+    }
+
+    @Test
+    public void Squad_squadSize_isInt() {
+        Squad testSquad = new Squad("Olympus", 5, "Fight the titans");
+        assertEquals(5, testSquad.getmSize());
+    }
+
+    @Test
+    public void Squad_squadCause_isString() {
+        Squad testSquad = new Squad("Olympus", 5, "Fight the titans");
+        assertEquals("Fight the titans", testSquad.getmCause());
     }
 }
