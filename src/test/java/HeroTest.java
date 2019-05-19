424c55be54586f73f3b030328238cb1d6981edac
Zenas Muli <zenas2294@gmail.com>
2019-05-19 14:04:14 +0300

    All tests for Hero inputs passed with insertion of necesarry methods

--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -16,5 +16,27 @@ public class HeroTest {
     }
 
     @Test
-    public void Hero_
+    public void Hero_heroAge_isInt() {
+        Hero testHero = new Hero("Zeus", 24, "God of Lightning", "Cheats on Hera a lot");
+        assertEquals(24, testHero.getmMiaka());
+    }
+
+    @Test
+    public void Hero_heroStrength_isString() {
+        Hero testHero = new Hero("Zeus", 24, "God of Lightning", "Cheats on Hera a lot");
+        assertEquals("God of Lightning", testHero.getmStrength());
+    }
+
+    @Test
+    public void Hero_heroWeakness_isString() {
+        Hero testHero = new Hero("Zeus", 24, "God of Lightning", "Cheats on Hera a lot");
+        assertEquals("Cheats on Hera a lot", testHero.getmWeakness());
+    }
+
+    @Test
+    public void Hero_heroId_retrievesTheId() {
+//        Hero.clear();
+        Hero testHero = new Hero("Zeus", 24, "God of Lightning", "Cheats on Hera a lot");
+        assertEquals(1, testHero.getmId());
+    }
 }
