a94c22c644637e730b78e9d0851103b897e840c5
Zenas Muli <zenas2294@gmail.com>
2019-05-19 16:43:03 +0300

    Added hero.vtl to show a specific hero

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -51,5 +51,13 @@ public class App {
             return new ModelAndView(model, layout);
         }), new VelocityTemplateEngine());
 
+        get("/heroes/:id", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            Hero hero = Hero.find(Integer.parseInt(request.params(":id")));
+            model.put("heroes", hero);
+            model.put("template", "templates/hero.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
     }
 }
