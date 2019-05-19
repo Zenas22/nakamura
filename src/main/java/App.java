38ffe36d3b1fd122b0356379367f42977c1c7db4
Zenas Muli <zenas2294@gmail.com>
2019-05-19 15:43:46 +0300

    Added hero creation form to the hero-form.vtl

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -18,8 +18,10 @@ public class App {
         }), new VelocityTemplateEngine());
 
         get("/hero-form", ((request, response) -> {
-
-        }));
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/hero-form.vtl");
+            return new ModelAndView(model, layout);
+        }), new VelocityTemplateEngine());
 
     }
 }
