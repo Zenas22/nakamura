0717d26199802380be30e32bd14b004f9a59280f
Zenas Muli <zenas2294@gmail.com>
2019-05-19 16:28:16 +0300

    Made Heroes.vtl to show all heroes and tweeked index.vtl to lead to a list of heroes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -13,6 +13,7 @@ public class App {
 
         get("/", ((request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
+            model.put("heroes", request.session().attribute("heroes"));
             model.put("template", index);
             return new ModelAndView(model, layout);
         }), new VelocityTemplateEngine());
@@ -20,6 +21,33 @@ public class App {
         get("/hero-form", ((request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             model.put("template", "templates/hero-form.vtl");
+            return new ModelAndView(model, layout);
+        }), new VelocityTemplateEngine());
+
+        get("/heroes", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("heroes", Hero.getHeroes());
+            model.put("template", "templates/heroes.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+        post("/heroes", ((request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            ArrayList<Hero> heroes = request.session().attribute("heroes");
+            if (heroes == null){
+                heroes = new ArrayList<Hero>();
+                request.session().attribute("heroes", heroes);
+            }
+
+            String jina = request.queryParams("jina");
+            int miaka = Integer.parseInt(request.queryParams("miaka"));
+            String strength = request.queryParams("strength");
+            String weakness = request.queryParams("weakness");
+
+            Hero newHero = new Hero(jina,miaka,strength,weakness);
+            heroes.add(newHero);
+
+            model.put("template", "template/successful.vtl");
             return new ModelAndView(model, layout);
         }), new VelocityTemplateEngine());
 
