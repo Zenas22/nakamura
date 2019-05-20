b510eb12083252bd30778fbceb22658e963e6398
Zenas Muli <zenas2294@gmail.com>
2019-05-20 03:28:43 +0300

    Added squad form

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -59,5 +59,11 @@ public class App {
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
 
+        get("/squads/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/squad-form.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
     }
 }
